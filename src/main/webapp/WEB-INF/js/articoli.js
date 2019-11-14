4d7d14732521a213c1bb07e0516eef793ac1f354
Davide Gallinella <davide.gallinella@objectmethod.it>
2019-11-14 11:48:05 +0100

    implemented lotti e modifica articoli frontend

--- src/main/webapp/WEB-INF/js/articoli.js
+++ src/main/webapp/WEB-INF/js/articoli.js
@@ -3,10 +3,7 @@ function showArticoli() {
 	document.getElementById("documenti-content").style.display = "none";
 	console.log("MOSTRA ARTICOLI");
     var xmlhttp = new XMLHttpRequest();
-    var filtro = "";
-    if (document.getElementById("articoliFilter")!=null){
-    	 filtro = document.getElementById("articoliFilter").value;
-    	}
+    var filtro= document.getElementById("articoliFilter").value;
     
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
@@ -15,7 +12,7 @@ function showArticoli() {
             articoli = articoli.results;
 			var htmlGen = "<tr><th>Codice</th><th>Descrizione</th><th>Quantita Totale</th><th>Azione<th></tr>";
 			for(var a of articoli){
-				htmlGen+="<tr><td><a onClick="mostraLotto(a.id)">"+ a.codice + "</a></td><td><a onClick="mostraLotto(a.id)">"+ a.descrizione +"</a></td><td><a onClick="mostraLotto(a.id)">"+a.quantitaTot +"</a></td><td align="center"><a onClick="modificaArticolo(a.id)"><input type="button" value="Modifica"></a></td></tr>";
+				htmlGen+="<tr><td><a onClick=\"mostraLotto("+a.id+")\">"+ a.codice + \"</a></td><td><a onClick=\"mostraLotto("+a.id+")\">"+ a.descrizione +"</a></td><td><a onClick=\"mostraLotto("+a.id+")\">"+a.quantitaTot +"</a></td><td align=\"center\"><p><input type=\"button\" value=\"Modifica\" onClick=\"modificaArticolo("+a.id+","+a.codice+","+a.descrizione+")\"></p></td></tr>";
 			}
 			document.getElementById("articoli-content").style.display = "block";
 			document.getElementById("articoli-table").innerHTML = htmlGen;
@@ -26,5 +23,96 @@ function showArticoli() {
 			true);
     xmlhttp.send(filtro);
     console.log("Ho mandato la chiamata");
+}
+
+function mostraAggiungiArticolo(){
+	document.getElementById("articoli-content").style.display = "none";
+	document.getElementById("aggiungi-articolo-container").style.display = "block";
+}
+
+function inserisciArticolo(){
+	var codiceArticolo=document.getElementById("codiceArticoloIns").value;
+	var descrizioneArticolo=document.getElementById("descrizioneArticoloIns").value;
+	var articolo = {id:0,codice:codiceArticolo,descrizione:descrizioneArticolo};
+	var articoloJSON = JSON.stringify(articolo);
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+            console.log("Ho ricevuto la risposta");
+            var esito = JSON.parse(this.responseText);
+            esito = esito.results;
+            if (esito.localCompare("OPERAZIONE ESEGUITA CON SUCCESSO")==0){
+            	document.getElementById("aggiungi-articolo-container").style.display="none";
+            	showArticoli();
+            	alert("ARTICOLO INSERITO CORRETTAMENTE"); 	
+            }else{
+            	alert("INSERIMENTO NON VALIDO");
+            } 
+		}
+    };
+    
+    xmlhttp.open("POST", "http://localhost:8080/api/articoli/save",
+			true);
+    xmlhttp.send(articoloJSON);
+    console.log("Ho mandato la chiamata");	
+}
+
+function mostraModificaArticolo(var idArticolo, var codArticolo, var descrizArticolo){
+	document.getElementById("articoli-content").style.display = "none";
+	document.getElementById("codiceArticoloIns").value =codArticolo;
+	document.getElementById("descrizioneArticoloIns").value =descrizArticolo;
+	document.getElementById("idModArticolo").value= idArticolo;
+	document.getElementById("modifica-articolo-container").style.display = "block";
+}
+
+function modificaArticolo(){
+	var codiceArticolo=document.getElementById("codiceArticoloIns").value;
+	var descrizioneArticolo=document.getElementById("descrizioneArticoloIns").value;
+	var idArticolo = document.getElementById("idModArticolo").value;
+	var articolo = {id:idArticolo,codice:codiceArticolo,descrizione:descrizioneArticolo};
+	var articoloJSON = JSON.stringify(articolo);
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+            console.log("Ho ricevuto la risposta");
+            var esito = JSON.parse(this.responseText);
+            esito = esito.results;
+            if (esito.localCompare("OPERAZIONE ESEGUITA CON SUCCESSO")==0){
+            	document.getElementById("aggiungi-articolo-container").style.display="none";
+            	showArticoli();
+            	alert("ARTICOLO MODIFICATO CORRETTAMENTE"); 	
+            }else{
+            	alert("MODIFICA NON VALIDA");
+            } 
+		}
+    };
+    
+    xmlhttp.open("POST", "http://localhost:8080/api/articoli/save",
+			true);
+    xmlhttp.send(articoloJSON);
+    console.log("Ho mandato la chiamata");	
+}
+
+function mostraLotto(idArticolo){
+	document.getElementById("articoli-content").style.display = "none";
+	console.log("MOSTRA LOTTI");
+    var xmlhttp = new XMLHttpRequest();    
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+            console.log("Ho ricevuto la risposta");
+            var articoli = JSON.parse(this.responseText);
+            lotti = lotti.results;
+			var htmlGen = "<tr><th>Codice Lotto</th><th>Quantita</th></tr>";
+			for(var l of lotti){
+				htmlGen+="<tr><td>"+l.codice+"</td><td>"+l.quantita+"</td></tr>";
+			}
+			document.getElementById("lotti-content").style.display = "block";
+			document.getElementById("lotti-table").innerHTML = htmlGen;
+		}
+    };
+    
+	xmlhttp.open("GET", "http://localhost:8080/api/lotti/list",
+			true);
+    xmlhttp.send(idArticolo);
+    console.log("Ho mandato la chiamata");
+    
 }
 
--- src/main/webapp/WEB-INF/jsp/frontEnd.jsp
+++ src/main/webapp/WEB-INF/jsp/frontEnd.jsp
@@ -6,6 +6,7 @@
 <style>
 </style>
 <script src="../js/articoli.js"></script>
+<script src="../js/documenti.js"></script>
 <link rel="stylesheet" type="text/css" href="../css/style.css">
 <meta charset="ISO-8859-1">
 <title>PaginaJSP</title>
@@ -21,32 +22,48 @@
 	<div id="main-content">
 		<div id="articoli-content" style="display: none;">
 			<div id="articoli-filter">
-				<b>Filtra Articoli:</b><input type= "text" id="articoliFilter"><br>
-					<input type="button" onClick="showArticoli()"><br><br>
+				<b>Filtra Articoli:</b><input type="text" id="articoliFilter"><br>
+				<input type="button" onClick="showArticoli()"><br> <br>
 			</div>
 			<div id="insertArticle-content">
 				<p align="left">
-					<input type=button onClick="aggiungiArticolo()"
+					<input type=button onClick="mostraAggiungiArticolo()"
 						value="Aggiungi Articolo">
 				</p>
 			</div>
 			<div id="articoli-table-container">
 				<h1 align="left">Tabella Articoli:</h1>
-				<table id="articoli-table">			
-				<!-- righe generate via JS -->
+				<table id="articoli-table">
+					<!-- righe generate via JS -->
 				</table>
 			</div>
+			<div id="aggiungi-articolo-container" style="display: none;">
+				<h1>Inserisci Articolo</h1>
+				Codice: <input type=text id="codiceArticoloIns"><br>
+				Descrizione: <input type=text id="descrizioneArticoloIns"><br>
+				<input type="button" onClick="inserisciArticolo()" value="OK">
+			</div>
+			<div id="modifica-articolo-container">
+				<h1>Modifica Articolo</h1>
+				Codice: <input type=text id="codiceArticoloIns"><br>
+				Descrizione: <input type=text id="descrizioneArticoloIns"><br>
+				<input id="idModArticolo" type="hidden"> <input
+					type="button" onClick="modificaArticolo()" value="OK">
+			</div>
+		</div>
+		<div id="lotti-content" style="display: none;">
+		    <h1>Elenco Lotti</h1><br><br>
+			<table id="lotti-table" style="width: 100%">
+			</table>
 		</div>
 		<div id="documenti-content" style="display: none;">
 			<div id="documenti-filter">
-				<p align ="left">Filtro: </p>
-				<select id="profili-documento" required="required" style='margin:5px'>
-				</select>
-				Da: <input type="date" id="data1">
-				A: <input type = "date" id="data2">
-				<br>
-				<input type = "button" onClick="showDocumenti()" value = "VAI">
-				<br><br>
+				<p align="left">Filtro:</p>
+				<select id="profili-documento" required="required"
+					style='margin: 5px'>
+				</select> Da: <input type="date" id="data1"> A: <input type="date"
+					id="data2"> <br> <input type="button"
+					onClick="showDocumenti()" value="VAI"> <br> <br>
 			</div>
 			<div id="insertDocument-content">
 				<p align="left">
@@ -55,12 +72,11 @@
 				</p>
 			</div>
 			<div id="documenti-table-container">
-			<h1 align="left">Tabella Documenti:</h1>
-				<table id="documenti-table">		
-				<!--righe generate via JS -->		
+				<h1 align="left">Tabella Documenti:</h1>
+				<table id="documenti-table">
+					<!--righe generate via JS -->
 				</table>
 			</div>
 		</div>
-	</div>
 </body>
 </html>
