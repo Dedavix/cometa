d24581d48d6f27f38aad97ae186b16ac2eb1a6df
Ivan Maltese <ivan.maltese@objectmethod.it>
2019-12-02 17:27:56 +0100

    fixed startup bugs

--- src/main/java/it/objectmethod/cometa/repositories/DocumentoRepository.java
+++ src/main/java/it/objectmethod/cometa/repositories/DocumentoRepository.java
@@ -13,12 +13,12 @@ import it.objectmethod.cometa.model.Documento;
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Integer> {
 	
-	@Query("select max(d.progressivo) Documento d where year(d.data)=?1 and d.idProfilo = ?2 group by d.idProfilo, year(d.data)")
+	@Query("select max(d.progressivo) from Documento d where year(d.data)=?1 and d.profilo.id = ?2")
 	public Integer getLastProgressivo(String anno, int idProfilo);
 	
-	@Query(value ="select d.id from Documento d where d.progressivo = ?1 and d.idProfilo = ?2 and d.data = ?3", nativeQuery = true)
+	@Query(value ="select d.id from Documento d where d.progressivo = ?1 and d.profilo.id = ?2 and d.data = ?3")
 	public Integer getDocumentId(int progressivo, int idProfilo, Date data);
 	
-	@Query("select d from documenti d where (:idProfilo = 0 or d.id = :idProfilo) AND d.data BETWEEN :dataFrom and :dataTo ")
+	@Query("select d from Documento d where (:idProfilo = 0 or d.profilo.id = :idProfilo) AND d.data BETWEEN :dataFrom and :dataTo ")
 	public List<Documento> findFilteredDocument(@Param("idProfilo")int idProfilo, @Param("dataFrom") Date data1, @Param("dataTo") Date data2);
 }
--- src/main/java/it/objectmethod/cometa/repositories/RigaDocumentoRepository.java
+++ src/main/java/it/objectmethod/cometa/repositories/RigaDocumentoRepository.java
@@ -3,6 +3,7 @@ package it.objectmethod.cometa.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import it.objectmethod.cometa.model.RigaDocumento;
@@ -10,6 +11,7 @@ import it.objectmethod.cometa.model.RigaDocumento;
 @Repository
 public interface RigaDocumentoRepository extends JpaRepository<RigaDocumento, Integer>{
 	
-	public List<RigaDocumento> findByIdDocumento(int idDocumento);
+	@Query("select r from RigaDocumento r where r.documento.id = ?1")
+	public List<RigaDocumento> findByIdDocumentoselect(int idDocumento);
 
 }
